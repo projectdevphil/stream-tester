0ace3cf357732bbd78bec956d7835ca8786080e0
Dangerouxxx <arbiemapalopeligro@gmail.com>
2025-11-21 10:41:39 +0800

    Update getChannels.js

--- js/getChannels.js
+++ js/getChannels.js
@@ -19,6 +19,9 @@
     "Sample_6_(MP4_compatible_with_HLS)": {
         "url": "https://projectdevphil.github.io/stream-tester/assets/samples/sample_4.mp4"
     },
+    "Boots S1 - Preview (Netflix TV Series)": {
+        "url": "https://occ-0-1763-395.1.nflxso.net/so/soa3/926/1973854540602986496.mp4?v=1&e=1763730070&t=M70_vUWeZ8y-LclACLaClxPxSPY"
+    },
     "Boots S1 - Trailer (Netflix TV Series)": {
         "url": "https://imdb-video.media-imdb.com/vi1258604569/1434659529640-260ouz-1758565194256.mp4?Expires=1763772581&Signature=SZgv3oMJSkHQAOPoGagYUsJrbpSgAIY11gIlmIwdhOcnDwsEZYgQKpVCLDfyayrpog6kVa36DyeP54K9YiIxVodpR9uHtgG1tojGeokfHHUNR8lohOMn~vxmT-m1NRY0F34O-ovd1J0HMgs1saMN-dr54lLE0verC4qVJwW0o2Jdqo4iUrPgfcXP-tCKkLEHetuDITfFHCTPOC~wueVpUSSIZrO0ewnX-N3mi6Y88GBQkGKiErZERoDStUFPkLVCwhCNoUqH3xI8MXLCmwT1mk471T9OSqvGNbEo3FytRF686OXjlZdO-e2fN1alMiFJqeLcFzXRnnFIIfKai2OD2Q__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA"
     },
